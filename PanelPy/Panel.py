3d61d724fa25179e9ad37edadf9e02a74dac77e4
Colin Fraser <colin.fraser@enernoc.com>
2015-11-11 23:13:35 -0800

    fixed within transform

--- PanelPy/Panel.py
+++ PanelPy/Panel.py
@@ -21,7 +21,7 @@ class Panel:
         self.data = data.copy()
         self.data.index = [data[i], data[t]]
         self.i = i
-        self.j = t
+        self.t = t
 
         if not keep_index:
             self.data.drop([i, t], axis=1, inplace=True)
@@ -34,9 +34,32 @@ class Panel:
         return self.N == self.n * self.i
 
     def within(self, *args):
+        """
+        Performs the 'within transformation' on the variables chosen in *args. If no variables are selected,
+        the transformation is performed on all variables
+        :param args:
+        :return:
+        """
+        within = lambda x: x - x.mean()
         if not args:
-            return self.data.groupby(level=self.i).mean()
-        return self.data[list(args)].groupby(level=self.i).mean()
+            return self.data.groupby(level=self.i).transform(within)
+        return self.data[list(args)].groupby(level=self.i).transform(within)
+
+    @property
+    def panel_summary(self):
+        stats = []
+        balance = 'Balanced' if self.balanced else 'Unbalanced'
+        balance += ' panel.'
+        stats.append(balance)
+        stats.append('Group variable: %s' % self.i)
+        stats.append('Time variable: %s' % self.t)
+        stats.append('n = %i' % self.n)
+        stats.append('T = %i' % self.T)
+        stats.append('N = %i' % self.N)
+        return '\n'.join(stats)
+
+    def __str__(self):
+        return '\n\n'.join([self.panel_summary, self.data.__str__()])
 
 
 
