d1ef1770270eac831164e688e3b55a88652a5511
Colin Fraser <colin.fraser@enernoc.com>
2015-11-19 14:20:24 -0800

    all tests pass

--- regressions/__init__.py
+++ regressions/__init__.py
@@ -1,3 +1,1 @@
 __author__ = 'colin.fraser'
-
-from .config import *
--- regressions/tests/test_RDataFrame.py
+++ regressions/tests/test_RDataFrame.py
@@ -103,6 +103,7 @@ class TestRDataFrame(TestCase):
         r = rdf.regress(f, cluster='FIRM')
         self.assertEqual(r.vce, 'cluster')
         with self.assertRaises(TypeError):
+            # noinspection PyUnusedLocal
             r = rdf.regress(f, cluster=True)
         rdf2 = rdf.copy(deep=True)
         rdf2.xtset('FIRM', 'YEAR')
@@ -118,4 +119,15 @@ class TestRDataFrame(TestCase):
 
         for k in ['F', 'C', 'Intercept']:
             self.assertAlmostEqual(r.se[k], stata_results['se'][k], places=5)
+
+    def test_properties(self):
+        rdf = self.setup_rdf()
+        rdf.xtset('FIRM', 'YEAR')
+        p = rdf.panel_attributes
+        self.assertEqual(p.i, 'FIRM')
+        self.assertEqual(p.t, 'YEAR')
+        self.assertEqual(p.n, 10)
+        self.assertEqual(p.T, 20)
+        self.assertEqual(p.N, 200)
+        self.assertTrue(rdf.balanced)
 
